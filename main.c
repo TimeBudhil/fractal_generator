9dca8de125e51f8047b8edb84081f7264539e7d3
TimeBudhil <71571047+TimeBudhil@users.noreply.github.com>
2024-12-14 22:38:47 -0600

    Commented out the saving of images

--- main.c
+++ main.c
@@ -172,6 +172,10 @@ SDL_Color hsbToRgb(float hue, float saturation, float brightness);
  * @kevin please commment!, and add this not only to this header call but also to the actual function below
  */
 void heatmap(float minimum, float maximum, float val, int* r, int* g, int* b);
+
+
+/**taken from the interwebs */
+//void save_texture(SDL_Renderer *ren, SDL_Texture *tex, const char *filename);
 //clang main.c -I/opt/homebrew/include/SDL2 -L/opt/homebrew/lib -lSDL2 -o game
 int main(int argc, char* argv[]) {
 
@@ -339,6 +343,9 @@ int main(int argc, char* argv[]) {
                         which = 3;
                         printf("which: %d\n", which);
                         break;
+                    //case SDLK_s: 
+                        //save_texture(renderer,)
+                        //break;
                     case SDLK_r:
                         // Reset to original view
                         centerX = 0.0;
@@ -749,6 +756,92 @@ double scale_number(double cv, double min, double max, double nmin, double nmax)
     double new_number = ((cv * range2)/range1) + (nmin);
     return new_number;
 }
+
+/*
+ * taken from the web
+ 
+void save_texture(SDL_Renderer *ren, SDL_Texture *tex, const char *filename){
+    SDL_Texture *ren_tex;
+    SDL_Surface *surf;
+    int st;
+    int w;
+    int h;
+    int format;
+    void *pixels;
+
+    pixels  = NULL;
+    surf    = NULL;
+    ren_tex = NULL;
+    format  = SDL_PIXELFORMAT_RGBA32;
+
+    // Get information about texture we want to save 
+    st = SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+    if (st != 0) {
+        SDL_Log("Failed querying texture: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    ren_tex = SDL_CreateTexture(ren, format, SDL_TEXTUREACCESS_TARGET, w, h);
+    if (!ren_tex) {
+        SDL_Log("Failed creating render texture: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    //
+    // Initialize our canvas, then copy texture to a target whose pixel data we 
+    // can access
+    //
+    st = SDL_SetRenderTarget(ren, ren_tex);
+    if (st != 0) {
+        SDL_Log("Failed setting render target: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    SDL_SetRenderDrawColor(ren, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(ren);
+
+    st = SDL_RenderCopy(ren, tex, NULL, NULL);
+    if (st != 0) {
+        SDL_Log("Failed copying texture data: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    // Create buffer to hold texture data and load it 
+    pixels = malloc(w * h * SDL_BYTESPERPIXEL(format));
+    if (!pixels) {
+        SDL_Log("Failed allocating memory\n");
+        goto cleanup;
+    }
+
+    st = SDL_RenderReadPixels(ren, NULL, format, pixels, w * SDL_BYTESPERPIXEL(format));
+    if (st != 0) {
+        SDL_Log("Failed reading pixel data: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    // Copy pixel data over to surface 
+    surf = SDL_CreateRGBSurfaceWithFormatFrom(pixels, w, h, SDL_BITSPERPIXEL(format), w * SDL_BYTESPERPIXEL(format), format);
+    if (!surf) {
+        SDL_Log("Failed creating new surface: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    // Save result to an image 
+    st = SDL_SaveBMP(surf, filename);
+    if (st != 0) {
+        SDL_Log("Failed saving image: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    SDL_Log("Saved texture as BMP to \"%s\"\n", filename);
+
+cleanup:
+    SDL_FreeSurface(surf);
+    free(pixels);
+    SDL_DestroyTexture(ren_tex);
+}
+*/
+
 
 /**
  * Notes from Ellie
